773f48d6b4701c36fe64b88d399b8520d279b6c5
AayushPalak <30636275+AayushPalak@users.noreply.github.com>
2020-12-11 23:16:02 +0530

    Add files via upload

--- src/main/java/com/hackerrank/stocktrade/model/Trade.java
+++ src/main/java/com/hackerrank/stocktrade/model/Trade.java
@@ -1,14 +1,44 @@
 package com.hackerrank.stocktrade.model;
 
+import java.io.Serializable;
 import java.sql.Timestamp;
 
-public class Trade {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "trade")
+public class Trade implements Serializable {
+	
+	static final long serialVersionUID = 123423432;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+	
+	@Column(name="type")
     private String type;
+	
+	@OneToOne
+	@JoinColumn(name="user_id", referencedColumnName="id")
     private User user;
+    
+    @Column(name="symbol")
     private String symbol;
+    
+    @Column(name="shares")
     private Integer shares;
+    
+    @Column(name="price")
     private Float price;
+    
+    @Column(name="timestamp")
     private Timestamp timestamp;
     
     public Trade() {
--- src/main/java/com/hackerrank/stocktrade/model/User.java
+++ src/main/java/com/hackerrank/stocktrade/model/User.java
@@ -1,7 +1,25 @@
 package com.hackerrank.stocktrade.model;
 
-public class User {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+	
+	static final long serialVersionUID = 578563520;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+	
+	@Column(name="name")
     private String name;
     
     public User() {
